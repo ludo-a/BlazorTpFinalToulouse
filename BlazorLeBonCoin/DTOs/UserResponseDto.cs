96e363493e8b882c090a57b15f00788fce550c55
Ludovic A <ludovic.adach@outlook.fr>
2022-12-07 11:14:16 +0100

    correction bug requete creation User

--- BlazorLeBonCoin/DTOs/UserResponseDto.cs
+++ BlazorLeBonCoin/DTOs/UserResponseDto.cs
@@ -6,7 +6,6 @@ public class UserResponseDto
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }   
-    public string Password { get; set; }
     
-    public bool StatusUser { get; set; }
+    public string Role { get; set; }
 }
