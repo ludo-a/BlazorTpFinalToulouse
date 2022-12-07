4161110250b78f8444ed2b040b713feb2578b283
Ludovic A <ludovic.adach@outlook.fr>
2022-12-07 10:18:23 +0100

    deleted cached /object

--- BlazorLeBonCoin/Program.cs
+++ BlazorLeBonCoin/Program.cs
@@ -7,6 +7,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://api-leboncoin-toulouse.azurewebsites.net") });
 builder.Services.AddSingleton<LoginService>();
 await builder.Build().RunAsync();
